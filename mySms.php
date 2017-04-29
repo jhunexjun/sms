22effe8859466fa4cf24cb8411973a71e22f8147
jhunexjun <jdream_catcher@yahoo.com>
2017-04-30 01:54:53 +0800

    Fixed.

--- mySms.php
+++ mySms.php
@@ -8,6 +8,8 @@ print "Set device: " . $sms->setDevice('/dev/ttyUSB2') . "\n";
 print "Open device: " . $sms->openDevice() . "\n";
 print "Set baud rate: " . $sms->setBaudRate(115200) . "\n";
 print "Sent message: " . $sms->sendSMS('+639332162333', 'I miss you.') . "\n";
+// print "Sent message: " . $sms->sendSMS('+639472609815', 'I miss you.') . "\n";
+// print "Sent message: " . $sms->sendSMS('+639773126821', 'Fuck up!!!') . "\n";
 $sms->sendCmd("ATi\r");
 print $sms->getDeviceResponse() . "\n";
 print "Device closed: " . $sms->closeDevice() . "\n";
